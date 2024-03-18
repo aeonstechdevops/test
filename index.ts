5f90b5f5f405fdf8772897b6a66f3bd823e29232
Viren <virubhosale112@gmail.com>
2024-03-19 00:42:14 +0530

    Test

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
+import model from "./model";
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello I am Server!");
+mongoose
+  .connect(
+    "mongodb+srv://aeonstechdevops:aeonstechdevops@cluster0.3wqhluk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected to DB"))
+  .catch((err: any) => console.error(err));
+
+app.get("/", async (req: any, res: any) => {
+  console.log(await model.create({ name: "Server " + Date.now() }));
+  return res.send("Hello I am Server!");
 });
 
 app.listen(3000, () => {
